91ee2a97cdaab09eb86fc3ea1fb738dd638e1052
Daya <212627323@logon.ds.ge.com>
2019-05-12 12:36:59 -0400

    updates rule types

--- promotions-service/rules-engine/rule-types.js
+++ promotions-service/rules-engine/rule-types.js
@@ -14,10 +14,18 @@ module.exports = Object.freeze({
 	 * Example of promotion types covered by rule type BUY_COUNT_OF_X_GET_COUNT_OF_Y_FREE
 	 * "Buy 3 google Homes for the price of 2"
 	 */
-	BUY_Y_OF_X_FOR_THE_PRICE_OF_Z: "buyYOfXForPriceOfZ",
+	BUY_Y_OF_X_FOR_THE_PRICE_OF_Z: {
+		id: "buyYOfXForPriceOfZ",
+		description: "Buy a quantity of an item for the price of a less quantity of the same item",
+		example: "Buy 3 google Homes for the price of 2"
+	},
 	/**
 	 * Example of promotion types covered by rule type BUY_MORE_THAN_COUNT_OF_X_GET_Y_PERCENT_DISCOUNT_ON_ALL_X
 	 * "Buying more than 3 Alexa speakers will have a 10% discount on all Alexa speakers"
 	 */
-	BUY_MORE_THAN_COUNT_OF_X_GET_Y_PERCENT_DISCOUNT_ON_ALL_X:"buyMoreThanCountOfXGetYPercentDiscountOnAllXs"
+	BUY_MORE_THAN_COUNT_OF_X_GET_Y_PERCENT_DISCOUNT_ON_ALL_X: {
+		id: "buyMoreThanCountOfXGetYPercentDiscountOnAllXs",
+		description: "Buy more than a quantity of an item and get a percentage discount for the price of all the same items",
+		example: "Buying more than 3 Alexa speakers will have a 10% discount on all Alexa speakers"
+	},
 });
